f147e6d07ba6d33750d7f95d3c550ccdb00e8831
Danny Bloemendaal <danny@bloemeland.nl>
2016-03-23 09:57:04 +0100

    rename

--- script_time_main.lua
+++ script_time_main.lua
@@ -1,7 +1,7 @@
 -- make sure we can find our modules
 local currentPath = debug.getinfo(1).source:match("@?(.*/)")
 scriptsFolderPath = currentPath .. 'scripts'
-package.path = package.path .. ';' .. currentPath .. '/?.lua'
+package.path = package.path .. ';' .. currentPath .. '?.lua'
 package.path = package.path .. ';' .. currentPath .. 'dzVents/?.lua'
 package.path = package.path .. ';' .. currentPath .. 'scripts/?.lua'
 
